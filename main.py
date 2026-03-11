249ff48562137028298988b6f85a2e8d9b525cd1
Hunter Gould <136615791+Gouldh@users.noreply.github.com>
2026-03-11 19:29:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ for ticker in tickers:
 
 market_caps = bl.get_market_caps(tickers)
 index_data = mv.download_stock_data(market_representation, training_start_date, training_end_date)
-index_return = (index_data['Adj Close'].iloc[-1] / index_data['Adj Close'].iloc[0]) - 1
+index_return = (index_data['Close'].iloc[-1] / index_data['Close'].iloc[0]) - 1
 
 # Calculate market returns for each asset
 market_returns = bl.get_market_returns(market_caps, index_return)
@@ -77,7 +77,7 @@ optimized_weights_ml_mv = mv.mean_variance_optimization(tickers, training_start_
 
 # Download market data for backtesting and calculate performance of each asset
 historical_data_backtest = mv.download_stock_data(tickers, backtesting_start_date, backtesting_end_date)
-daily_returns_backtest = historical_data_backtest['Adj Close'].pct_change()
+daily_returns_backtest = historical_data_backtest['Close'].pct_change()
 
 # Calculate the cumulative performance of the machine learning mean variance optimized portfolio
 portfolio_returns_ml_mv = daily_returns_backtest.dot(optimized_weights_ml_mv)
@@ -88,7 +88,7 @@ portfolio_returns_mv = daily_returns_backtest.dot(optimized_weights_mv)
 cumulative_returns_mv = (1 + portfolio_returns_mv).cumprod()
 
 # Download and calculate market index cumulative returns
-market_data = mv.download_stock_data(market_representation, backtesting_start_date, backtesting_end_date)['Adj Close']
+market_data = mv.download_stock_data(market_representation, backtesting_start_date, backtesting_end_date)['Close']
 market_returns = market_data.pct_change()
 cumulative_market_returns = (1 + market_returns).cumprod()
 
